67e0241dd629a3d15a1acc22cde9dc83806f3a4e
Alex Anokhin <olanokhin@gmail.com>
2025-04-23 16:58:38 +0200

    delete unnecessary comment

--- src/main/kotlin/com/wire/bots/domain/reminder/ReminderJobRepository.kt
+++ src/main/kotlin/com/wire/bots/domain/reminder/ReminderJobRepository.kt
@@ -1,7 +1,6 @@
 package com.wire.bots.domain.reminder
 
 import arrow.core.Either
-// import java.util.*
 
 interface ReminderJobRepository {
     fun scheduleReminderJob(reminder: Reminder): Either<Throwable, ReminderNextSchedule>
