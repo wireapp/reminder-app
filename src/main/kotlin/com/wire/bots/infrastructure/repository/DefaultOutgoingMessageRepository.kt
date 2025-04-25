b317ca037d79eb48d0bb099f0df7f4bac74536d9
Alex Anokhin <olanokhi@student.42heilbronn.de>
2025-04-25 14:35:01 +0200

    delete commented LoggerFactory

--- src/main/kotlin/com/wire/bots/infrastructure/repository/DefaultOutgoingMessageRepository.kt
+++ src/main/kotlin/com/wire/bots/infrastructure/repository/DefaultOutgoingMessageRepository.kt
@@ -8,14 +8,11 @@ import com.wire.bots.infrastructure.client.ConversationRemoteApi
 import com.wire.bots.infrastructure.toOutgoingMessage
 import jakarta.enterprise.context.ApplicationScoped
 import org.eclipse.microprofile.rest.client.inject.RestClient
-// import org.slf4j.LoggerFactory
 
 @ApplicationScoped
 class DefaultOutgoingMessageRepository(
     @RestClient val conversationRemoteApi: ConversationRemoteApi,
 ) : OutgoingMessageRepository {
-    // private val logger = LoggerFactory.getLogger(this::class.java)
-
     override fun sendMessage(
         conversationId: PlainConversationId,
         token: String,
